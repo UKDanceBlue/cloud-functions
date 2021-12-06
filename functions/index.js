5da6f4ef6bf36b14cd44a19fb2894b2eabcd7b50
Tag Howard <tag.howard1080@gmail.com>
2021-12-06 15:13:29 -0500

    Add a fundraising sync method
    I disabled the function until Sydney signs off and I get a real endpoint

--- functions/index.js
+++ functions/index.js
@@ -7,14 +7,16 @@ import { getAuth } from "firebase-admin/auth";
 
 import { Expo } from "expo-server-sdk";
 
+import fetch from "node-fetch";
+
 initializeApp();
 
 export const sendPushNotification = functions.https.onCall(async (data, context) => {
   const { notificationTitle, notificationBody, notificationData, notificationTtl } = data;
   const { email } = context.auth.token;
 
-  const notificationsConfig = await getFirestore().doc("configs/notifications").get();
-  const emails = notificationsConfig.get("allowedEmails");
+  const notificationsConfig = await getFirestore().doc("configs/notifications").get().data;
+  const emails = notificationsConfig.allowedEmails;
   if (!emails.includes(email)) {
     return {
       status: "error",
@@ -145,4 +147,92 @@ export const sweepOldAccounts = functions.https.onRequest((req, res) => {
     response.status = "OK";
   }
   res.json(response);
+});
+
+// This function will run on february 31st (it won't)
+export const syncDBFunds = functions.pubsub.schedule("0 0 5 31 2 ?").onRun(async () => {
+  // Get config info from firebase
+  const dbFundsSyncConfig = (await getFirestore().doc("configs/db-funds-sync").get()).data();
+  const currentFiscalYears = dbFundsSyncConfig.currentFiscalYears;
+  const authToken = dbFundsSyncConfig.authToken;
+
+  // A map of active teams and their totals to be loaded from the database
+  const teamTotals = {};
+  if (Array.isArray(currentFiscalYears)) {
+    for (const fiscalYear of currentFiscalYears) {
+      let json;
+      try {
+        // Load the given fiscal year from the database
+        const response = await fetch(
+          `https://dancebluefundspreview.uky.edu/api/report/teamtotals/${fiscalYear}`,
+          {
+            headers: {
+              method: "GET",
+              "X-AuthToken": authToken,
+            },
+          }
+        );
+        json = await response.json();
+      } catch (error) {
+        functions.logger.error(
+          `Error when fetching data from fiscal year ${fiscalYear}.`,
+          `Skipping fiscal year ${fiscalYear}.`,
+          error
+        );
+        continue;
+      }
+
+      // Make sure the array is json, if not then the database probably rejected our auth token
+      if (Array.isArray(json)) {
+        for (let i = 0; i < json.length; i++) {
+          // Ensure that each element of the array has a team name and is active
+          if (json[i].Team && json[i].Active) {
+            if (teamTotals[json[i].Team]) {
+              // If the team had a total stored in the last fiscal year, just add to it
+              teamTotals[json[i].Team] = teamTotals[json[i].Team] + json[i].Total;
+            } else {
+              // Otherwise create a spot for them
+              teamTotals[json[i].Team] = json[i].Total;
+            }
+          } else {
+            functions.logger.info(
+              `Error when trying to parse element ${i} of fiscal year ${fiscalYear}: *element is invalid*.`,
+              `Skipping element ${i}.`,
+              json
+            );
+            continue;
+          }
+        }
+      } else {
+        functions.logger.warn(
+          `Error when trying to parse fiscal year ${fiscalYear}: *response is not an array*.`,
+          `Skipping fiscal year ${fiscalYear}.`,
+          json
+        );
+        continue;
+      }
+    }
+  }
+
+  // Get a reference to the teams collection and get an array of all the teams
+  const firebaseTeams = (await getFirestore().collection("teams").get()).docs;
+  // Iterate through the array
+  firebaseTeams.forEach(async (queryDocumentSnapshot) => {
+    // Get team name as stored in the firebase docuement
+    const teamName = queryDocumentSnapshot.get("name");
+    // If the teamToals object has an element with the name of the team loaded from firebase then upload their total to firebase
+    if (teamTotals[teamName]) {
+      queryDocumentSnapshot.ref
+        // Merge the total into the team's document
+        .set({ fundraisingTotal: teamTotals[teamName] }, { merge: true })
+        .then(
+          (result) =>
+            functions.logger.info(
+              `Set *fundraisingTotal* of ${teamName} to ${teamTotals[teamName]}.`
+            ),
+          (reason) =>
+            functions.logger.error(`Failed to set *fundraisingTotal* of ${teamName}.`, reason)
+        );
+    }
+  });
 });
