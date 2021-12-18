c89547480d04063d39e6bccff27451aa060ce3b1
Tag Howard <tag.howard1080@gmail.com>
2021-12-17 22:48:45 -0500

    Add an https function to write a log message

--- functions/index.js
+++ functions/index.js
@@ -423,3 +423,35 @@ export const importSpiritPoints = functions.https.onRequest(async (req, res) => 
     res.end();
   }
 });
+
+export const writeLog = functions.https.onRequest(async (req, res) => {
+  try {
+    if (req.method === "POST") {
+      if (req.body && typeof req.body === "string") {
+        functions.logger.info(req.body.toString());
+        res.sendStatus(200).end();
+        return;
+      } else if (
+        req.body &&
+        typeof req.body === "object" &&
+        !Array.isArray(req.body) &&
+        typeof req.body.message === "string"
+      ) {
+        functions.logger.write({
+          message: req.body.message,
+          severity: req.body.severity ? req.body.severity : "INFO",
+        });
+        res.sendStatus(200).end();
+        return;
+      } else {
+        res.sendStatus(400).end();
+        return;
+      }
+    } else {
+      res.sendStatus(405).end();
+      return;
+    }
+  } catch (error) {
+    res.status(500).send(error).end();
+  }
+});
