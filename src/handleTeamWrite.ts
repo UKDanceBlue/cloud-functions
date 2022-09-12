bd523203a67f2fcc030fa6e19b825e225940decd
Tag Howard <tag.howard1080@gmail.com>
2022-09-12 12:13:05 -0400

    Add a firestore function to mirror team changes

---
+++ src/handleTeamWrite.ts
@@ -0,0 +1,24 @@
+import { getFirestore } from "firebase-admin/firestore";
+import { firestore as functionsFirestore } from "firebase-functions";
+
+export default functionsFirestore.document("/spirit/teams/documents/{teamId}").onWrite(async (change, context) => {
+  // Mirror the fields "name", "teamClass", and totalPoints from /spirit/teams/documents/{teamId} to /spirit/teams.basicInfo.{teamId}
+  const teamId = context.params.teamId as unknown;
+  if (typeof teamId !== "string") {
+    return;
+  }
+
+  const { name, teamClass, totalPoints } = change.after.data() ?? {};
+  if (typeof name !== "string" || typeof teamClass !== "string" || typeof totalPoints !== "number") {
+    return;
+  }
+
+  const batch = getFirestore().batch();
+  const basicInfoDoc = getFirestore().doc(`/spirit/teams/basicInfo/${teamId}`);
+  batch.set(basicInfoDoc, {
+    name,
+    teamClass,
+    totalPoints,
+  }, { merge: true });
+  await batch.commit();
+});
