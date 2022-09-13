e79d7e7c221907f7f6821fd8e19b0b9aca015347
Tag Howard <tag.howard1080@gmail.com>
2022-09-13 10:59:15 -0400

    Add logging for document watchers

--- src/handleOpportunityWrite.ts
+++ src/handleOpportunityWrite.ts
@@ -1,20 +1,32 @@
 import { Timestamp, getFirestore } from "firebase-admin/firestore";
-import { firestore as functionsFirestore } from "firebase-functions";
+import { firestore as functionsFirestore, logger } from "firebase-functions";
 
 export default functionsFirestore.document("/spirit/opportunities/documents/{opportunityId}").onWrite(async (change, context) => {
   // Mirror the fields "date" and "name" from /spirit/opportunities/documents/{opportunityId} to /spirit/opportunities.basicInfo.{opportunityId}
   const opportunityId = context.params.opportunityId as unknown;
   if (typeof opportunityId !== "string") {
+    logger.log("opportunityId is not a string, aborting");
     return;
   }
 
   const { name, date } = change.after.data() ?? {};
   if (typeof name !== "string" || !(date instanceof Timestamp)) {
+    logger.log("name or date is not a string or a Timestamp, aborting");
     return;
   }
 
   const batch = getFirestore().batch();
   const basicInfoDoc = getFirestore().doc("/spirit/opportunities");
   batch.set(basicInfoDoc, {[opportunityId]: { name, date }}, { merge: true });
-  await batch.commit();
+  logger.debug(`Added "Set /spirit/opportunities.${opportunityId}" to batch`);
+
+  try {
+    logger.debug("Committing batch");
+    await batch.commit();
+    logger.debug("Batch committed");
+  } catch (error) {
+    logger.error(error);
+  } finally {
+    logger.debug("Done");
+  }
 });
--- src/handleSpiritPointEntryWrite.ts
+++ src/handleSpiritPointEntryWrite.ts
@@ -1,5 +1,5 @@
 import { FieldPath, FieldValue, getFirestore,  } from "firebase-admin/firestore";
-import { firestore as functionsFirestore } from "firebase-functions";
+import { firestore as functionsFirestore, logger } from "firebase-functions";
 
 import { isSpiritPointEntry } from "./types/FirestoreSpiritPointEntry.js";
 
@@ -10,87 +10,117 @@ export default functionsFirestore.document("/spirit/teams/documents/{teamId}/poi
 
   const teamId = context.params.teamId as string;
 
+  logger.debug(`Extracted teamId: ${teamId} from context.params.teamId`);
+
   if (context.eventType === "google.firestore.document.create") {
+    logger.debug(`Detected create event for teamId: ${teamId}`);
+
     const entry = change.after.data();
 
     if (entry == null || entry.opportunityId == null) {
+      logger.log("Entry or entry.opportunityId is nullish, aborting");
       return;
     }
 
     // Verify the document's structure
     if (!isSpiritPointEntry(entry)) {
+      logger.log("Entry is not a valid SpiritPointEntry, aborting");
       return change.after.ref.delete();
     }
 
     // Make a copy to /spirit/opportunities/documents/{opportunityId}/pointEntries/{entryId}
     const opportunityEntryDocument = firestore.doc(`/spirit/opportunities/documents/${entry.opportunityId}/pointEntries/${change.after.id}`);
     batch.set(opportunityEntryDocument, entry);
+    logger.debug(`Added "Create opportunityEntryDocument: ${opportunityEntryDocument.path}" to batch`);
 
-    // Increment /spirit/teams/documents/{teamId}.totalPoints and /spirit/teams/documents/{teamId}.individualTotals
+    // Increment /spirit/teams/documents/{teamId}.totalPoints and /spirit/teams/documents/{teamId}.individualTotals.{linkblue}
     const teamInfoDocument = firestore.doc(`/spirit/teams/documents/${teamId}/info`);
     batch.update(teamInfoDocument, {
       totalPoints: FieldValue.increment(entry.points),
     });
+    logger.debug(`Added "Increment /spirit/teams/documents/${teamId}.totalPoints" to batch`);
     batch.update(teamInfoDocument, {
       [`individualTotals.${entry.linkblue}`]: FieldValue.increment(entry.points),
     });
+    logger.debug(`Added "Increment /spirit/teams/documents/${teamId}.individualTotals.${entry.linkblue}" to batch`);
 
     // Increment /spirit/teams.points.{teamId}
     const rootTeamsDoc = firestore.doc("/spirit/teams");
     batch.update(rootTeamsDoc, new FieldPath("points", teamId), FieldValue.increment(entry.points));
+    logger.debug(`Added "Increment /spirit/teams.points.${teamId}" to batch`);
 
     // Increment /spirit/opportunities/documents/{opportunityId}.totalPoints
     const opportunityInfoDocument = firestore.doc(`/spirit/opportunities/documents/${entry.opportunityId}`);
     batch.update(opportunityInfoDocument, {
       totalPoints: FieldValue.increment(entry.points),
     });
+    logger.debug(`Added "Increment /spirit/opportunities/documents/${entry.opportunityId}.totalPoints" to batch`);
 
     // Increment /spirit/info.totalPoints
     const rootInfoDoc = firestore.doc("/spirit/info");
     batch.update(rootInfoDoc, {
       totalPoints: FieldValue.increment(entry.points),
     });
+    logger.debug("Added \"Increment /spirit/info.totalPoints\" to batch");
   } else if(context.eventType === "google.firestore.document.delete") {
+    logger.debug(`Detected delete event for teamId: ${teamId}`);
+
     const entry = change.before.data();
 
     if (entry == null || entry.opportunityId == null) {
+      logger.log("Entry or entry.opportunityId was nullish, aborting");
       return;
     }
 
     // Verify the document's structure
     if (!isSpiritPointEntry(entry)) {
+      logger.log("Entry was not a valid SpiritPointEntry, aborting");
       return;
     }
 
     // Delete /spirit/opportunities/documents/{opportunityId}/pointEntries/{entryId}
     const opportunityEntryDocument = firestore.doc(`/spirit/opportunities/documents/${entry.opportunityId}/pointEntries/${change.before.id}`);
     batch.delete(opportunityEntryDocument);
+    logger.debug(`Added "Delete opportunityEntryDocument: ${opportunityEntryDocument.path}" to batch`);
 
-    // Decrement /spirit/teams/documents/{teamId}.totalPoints and /spirit/teams/documents/{teamId}.individualTotals
+    // Decrement /spirit/teams/documents/{teamId}.totalPoints and /spirit/teams/documents/{teamId}.individualTotals.{linkblue}
     const teamInfoDocument = firestore.doc(`/spirit/teams/documents/${teamId}/info`);
     batch.update(teamInfoDocument, {
       totalPoints: FieldValue.increment(-entry.points),
     });
+    logger.debug(`Added "Decrement /spirit/teams/documents/${teamId}.totalPoints" to batch`);
     batch.update(teamInfoDocument, {
       [`individualTotals.${entry.linkblue}`]: FieldValue.increment(-entry.points),
     });
+    logger.debug(`Added "Decrement /spirit/teams/documents/${teamId}.individualTotals.${entry.linkblue}" to batch`);
 
     // Decrement /spirit/teams.points.{teamId}
     const rootTeamsDoc = firestore.doc("/spirit/teams");
     batch.update(rootTeamsDoc, new FieldPath("points", teamId), FieldValue.increment(-entry.points));
+    logger.debug(`Added "Decrement /spirit/teams.points.${teamId}" to batch`);
 
     // Decrement /spirit/opportunities/documents/{opportunityId}.totalPoints
     const opportunityInfoDocument = firestore.doc(`/spirit/opportunities/documents/${entry.opportunityId}`);
     batch.update(opportunityInfoDocument, {
       totalPoints: FieldValue.increment(-entry.points),
     });
+    logger.debug(`Added "Decrement /spirit/opportunities/documents/${entry.opportunityId}.totalPoints" to batch`);
 
     // Decrement /spirit/info.totalPoints
     const rootInfoDoc = firestore.doc("/spirit/info");
     batch.update(rootInfoDoc, {
       totalPoints: FieldValue.increment(-entry.points),
     });
+    logger.debug("Added \"Decrement /spirit/info.totalPoints\" to batch");
   }
 
-  await batch.commit();
+  try {
+    logger.debug("Committing batch");
+    await batch.commit();
+    logger.debug("Batch committed");
+  } catch (error) {
+    logger.error(error);
+  } finally {
+    logger.debug("Done");
+  }
 });
