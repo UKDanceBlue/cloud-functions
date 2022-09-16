776978baed889e78c925449515a9424dd341911e
Tag Howard <tag.howard1080@gmail.com>
2022-09-16 12:45:08 -0400

    Handle team write correctly

--- src/handleTeamWrite.ts
+++ src/handleTeamWrite.ts
@@ -1,5 +1,5 @@
-import { getFirestore } from "firebase-admin/firestore";
-import { firestore as functionsFirestore } from "firebase-functions";
+import { FieldPath, FieldValue, getFirestore } from "firebase-admin/firestore";
+import { firestore as functionsFirestore, logger } from "firebase-functions";
 
 export default functionsFirestore.document("/spirit/teams/documents/{teamId}").onWrite(async (change, context) => {
   // Mirror the fields "name", "teamClass", and totalPoints from /spirit/teams/documents/{teamId} to /spirit/teams.basicInfo.{teamId}
@@ -8,13 +8,26 @@ export default functionsFirestore.document("/spirit/teams/documents/{teamId}").o
     return;
   }
 
-  const { name, teamClass, totalPoints } = change.after.data() ?? {};
-  if (typeof name !== "string" || typeof teamClass !== "string" || typeof totalPoints !== "number") {
-    return;
-  }
+  if (!change.after.exists) {
+    return getFirestore().collection("spirit").doc("teams").update({
+      [`basicInfo.${teamId}`]: FieldValue.delete(),
+    }).catch((error) => {
+      logger.error(error);
+    });
+  } else {
 
-  const batch = getFirestore().batch();
-  const basicInfoDoc = getFirestore().doc("/spirit/teams");
-  batch.set(basicInfoDoc, {[teamId]: { name, teamClass, totalPoints }}, { merge: true });
-  await batch.commit();
+    const { name, teamClass, totalPoints } = change.after.data() ?? {};
+    if (typeof name !== "string" || typeof teamClass !== "string" || typeof totalPoints !== "number") {
+      return;
+    } else {
+      const batch = getFirestore().batch();
+      const basicInfoDoc = getFirestore().doc("/spirit/teams");
+      batch.update(basicInfoDoc, new FieldPath("basicInfo", teamId), {
+        name,
+        teamClass,
+        totalPoints,
+      });
+      await batch.commit();
+    }
+  }
 });
